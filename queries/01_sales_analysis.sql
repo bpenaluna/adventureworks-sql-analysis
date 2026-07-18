fd4a114478ffc60369112ca51fd78465661ea5d3
bpenaluna <ben.penaluna@gmail.com>
2026-07-18 14:29:05 +0100

    Changes to SQL queries

--- queries/01_sales_analysis.sql
+++ queries/01_sales_analysis.sql
@@ -1,11 +1,18 @@
-SELECT
-	c.[Name] AS Territory,
-	YEAR(a.OrderDate) AS [Year],
-	ROUND(SUM(a.TotalDue / COALESCE(b.EndOfDayRate, 1)) / 1000000, 2) AS [Total Revenue in USD (Millions)]
-FROM Sales.SalesOrderHeader a
-	LEFT JOIN Sales.CurrencyRate b ON a.CurrencyRateID = b.CurrencyRateID
-	JOIN Sales.SalesTerritory c ON a.TerritoryID = c.TerritoryID
-WHERE a.[Status] NOT IN (4, 6)
-GROUP BY c.[Name], YEAR(a.OrderDate)
-HAVING SUM(a.TotalDue / COALESCE(b.EndOfDayRate, 1)) > 1000000
-ORDER BY [Total Revenue in USD (Millions)] DESC
+WITH revenue_over_mil AS (
+	SELECT
+		c.[Name] AS Territory,
+		YEAR(a.OrderDate) AS [Year],
+		ROUND(SUM(a.TotalDue / COALESCE(b.EndOfDayRate, 1)) / 1000000, 2) AS [Revenue (USD)]
+	FROM Sales.SalesOrderHeader a
+		LEFT JOIN Sales.CurrencyRate b ON a.CurrencyRateID = b.CurrencyRateID
+		JOIN Sales.SalesTerritory c ON a.TerritoryID = c.TerritoryID
+	WHERE a.[Status] NOT IN (4, 6)
+	GROUP BY c.[Name], YEAR(a.OrderDate)
+	HAVING SUM(a.TotalDue / COALESCE(b.EndOfDayRate, 1)) > 1000000
+)
+SELECT Territory, COUNT(*) AS [Count]
+FROM (
+	SELECT DISTINCT [Name] from Sales.SalesTerritory
+) t LEFT JOIN revenue_over_mil ON t.Name = revenue_over_mil.Territory
+GROUP BY Territory
+ORDER BY [Count] DESC, Territory
--- queries/03_territory_yearly_performances.sql
+++ queries/03_territory_yearly_performances.sql
@@ -1,23 +1,33 @@
-SELECT
-	c.[Name] AS Territory,
-	YEAR(a.OrderDate) AS [Year],
-	ROUND(SUM(a.TotalDue / COALESCE(b.EndOfDayRate, 1)) / 1000000, 2) AS [Total Revenue in USD (Millions)],
+WITH performance AS (
+	SELECT
+		c.[Name] AS Territory,
+		YEAR(a.OrderDate) AS [Year],
+		ROUND(SUM(a.TotalDue / COALESCE(b.EndOfDayRate, 1)), 2) AS [Revenue (USD)],
+		LAG(ROUND(SUM(a.TotalDue / COALESCE(b.EndOfDayRate, 1)), 2), 1, 0)
+			OVER(PARTITION BY c.[Name] ORDER BY YEAR(a.OrderDate)) AS [Last Year Revenue],
+		CASE
+			WHEN SUM(a.TotalDue / COALESCE(b.EndOfDayRate, 1)) > (
+				SELECT AVG(YearlyTotal)
+				FROM (
+					SELECT SUM(a1.TotalDue) AS YearlyTotal
+					FROM Sales.SalesOrderHeader a1
+						JOIN Sales.SalesTerritory c1 ON a1.TerritoryID = c1.TerritoryID
+					WHERE YEAR(a1.OrderDate) = YEAR(a.OrderDate)
+					GROUP BY c1.[Name]
+				) AS TerritoryTotals
+			) THEN 'Above Average'
+			ELSE 'Below Average'
+		END AS Performance
+	FROM Sales.SalesOrderHeader a
+		LEFT JOIN Sales.CurrencyRate b ON a.CurrencyRateID = b.CurrencyRateID
+		JOIN Sales.SalesTerritory c ON a.TerritoryID = c.TerritoryID
+	WHERE a.[Status] NOT IN (4, 6)
+	GROUP BY c.[Name], YEAR(a.OrderDate)
+)
+SELECT Territory, [Year], [Revenue (USD)],
 	CASE
-		WHEN SUM(a.TotalDue / COALESCE(b.EndOfDayRate, 1)) > (
-			SELECT AVG(YearlyTotal)
-			FROM (
-				SELECT SUM(a1.TotalDue) AS YearlyTotal
-				FROM Sales.SalesOrderHeader a1
-					JOIN Sales.SalesTerritory c1 ON a1.TerritoryID = c1.TerritoryID
-				WHERE YEAR(a1.OrderDate) = YEAR(a.OrderDate)
-				GROUP BY c1.[Name]
-			) AS TerritoryTotals
-		) THEN 'Above Average'
-		ELSE 'Below Average'
-	END AS Performance
-FROM Sales.SalesOrderHeader a
-	LEFT JOIN Sales.CurrencyRate b ON a.CurrencyRateID = b.CurrencyRateID
-	JOIN Sales.SalesTerritory c ON a.TerritoryID = c.TerritoryID
-WHERE a.[Status] NOT IN (4, 6)
-GROUP BY c.[Name], YEAR(a.OrderDate)
-ORDER BY [Year], [Total Revenue in USD (Millions)] DESC
+		WHEN [Last Year Revenue] = 0 THEN 0 ELSE ROUND(100 * [Revenue (USD)] / [Last Year Revenue], 2)
+	END AS [Percentage Change],
+	Performance
+FROM performance
+ORDER BY Territory DESC, [Year]
